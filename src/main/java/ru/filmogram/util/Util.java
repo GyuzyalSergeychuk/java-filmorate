be2568896a32889728e7a8ebe72e23974510a41d
Gyuzyal <gyuzyal.sergeychuk@yandex.ru>
2023-06-10 14:21:17 +0300

    style^ fix check

--- src/main/java/ru/filmogram/util/Util.java
+++ src/main/java/ru/filmogram/util/Util.java
@@ -1,23 +1,13 @@
 package ru.filmogram.util;
 
-import ru.filmogram.model.Genre;
 import ru.filmogram.model.Mpa;
-
-import java.util.List;
 
 public class Util {
 
-    public static List<Genre> checkNoGenre(String genreFilm) {
-        if (genreFilm == null) {
-            return List.of();
-        }
-        return null;
-    }
-
-    public static Mpa makeMpa(Long rating_id, String rating_name) {
+    public static Mpa makeMpa(Long ratingId, String ratingName) {
         return Mpa.builder()
-                .id(rating_id)
-                .name(rating_name)
+                .id(ratingId)
+                .name(ratingName)
                 .build();
     }
 }
